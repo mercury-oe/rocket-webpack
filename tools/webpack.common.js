d978dbc89d0a738819d3295ee29ef87c0de3cd1c
Oleg Ambrosevich <oleg.ambrosevich@yandex.ru>
2018-11-20 17:28:55 +0300

    added babel-loader config

--- tools/webpack.common.js
+++ tools/webpack.common.js
@@ -3,4 +3,24 @@ const path = require('path');
 const pkg = require(path.resolve('package.json'));
 const config = require(path.resolve('config'));
 
+const babelLoader = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+    },
+  },
+};
 
+const baseConfig = {
+  entry: config.entries,
+  module: {
+    rules: [
+      babelLoader,
+    ],
+  }
+};
+
+module.exports = baseConfig;
